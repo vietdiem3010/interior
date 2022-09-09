9614c703a94bbb421947adaa0a7112b0d3711234
Nguyen Viet Diem <nguyenvietdiem@gmail.com>
2022-09-09 14:09:22 +0700

    change ui

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.poster .slider').slick({
+      prevArrow: $('.arrow_left'),
+      nextArrow: $('.arrow_right'),
+      autoplay: true,
+      autoplaySpeed: 5000,
+      infinite: true
+    });
+    $('.slider-photos .slider').slick({
+      prevArrow: $('.arrow_left_slider'),
+      nextArrow: $('.arrow_right_slider'),
+    });
+    $('.clients__slider').slick();
+});
